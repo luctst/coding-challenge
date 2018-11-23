f994a6e7881c82a632509e655d12b8e9571877a3
luctst <luc.tostee@outlook.fr>
2018-11-23 16:01:43 +0100

    Organize array done

--- js/organize-array.js
+++ js/organize-array.js
@@ -1,23 +1,35 @@
 /**
  * Variables
  */
-const tab = [4, 2, 3, 1];
+const test = [4, 2, 3, 1];
+const test2 = [1, 2, 4, 3];
+const test3 = [10, 30, 25, 9, 60, 110, 105, 200, 190];
 
 /**
  * Declaration
  */
 const sort = (array) => {
-    for (let i = 0; i < array.length; i++) {
-        let el = array[i]; // Retrieve first iteration element.
-        let elAfter = array[i + 1]; // Retrieve next element by iteration.
+    let max = Math.max(...array); // Get higher value element.
+    let min = Math.min(...array); // Get lower value element.
+
+    while (array[array.length - 1] !== max || array[0] !== min) {
         
-        array[i + 1] = el;
-        array[i] = elAfter; 
+        for (let i = 0; i < array.length; i++) {
+            let el = array[i]; // Get first iteration element.
+            let elAfter = array[i + 1]; // Get next element by iteration.
+
+            if (el > elAfter) { // Change element position.
+                array[i + 1] = el;
+                array[i] = elAfter;
+            } 
+        }
     }
-    console.log(tab);
+    console.log(array);
 }
 
 /**
  * Execution
 */
-sort(tab);
+sort(test);
+sort(test2);
+sort(test3);

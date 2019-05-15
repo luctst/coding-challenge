c17aed49c699b0eaad7bd0e509527c93b371d3a4
Lucas Tostée <lucas.tostee@gmail.com>
2019-05-15 21:50:11 +0200

    Start details C1 challenge in Ruby

---
+++ challenges/C1-organize-array/organize-array.rb
@@ -0,0 +1,11 @@
+=begin
+* CHALLENGE - Given an array with X elements you should create a function who return a new array 
+ * with the numbers organized by ascending order.
+ * 
+ * DATA - Use this array.
+ * [4, 2, 3, 1]
+ * [1, 2, 4, 3]
+ * [10, 30, 25, 9, 60, 110, 105, 200, 190]
+ * 
+ * TEST - To defined.
+=end

551acc372971194f99cf19bf54dd9f2960cc0e82
Lucas Tostée <lucas.tostee@gmail.com>
2019-07-20 16:30:53 +0200

    Fixed getProduct() fucntion

--- test/get-product.test.js
+++ test/get-product.test.js
@@ -2,7 +2,7 @@ import test from "ava";
 import getProduct from "../challenges/get-products/getProducts";
 
 test("Test getProduct function with [1, 2, 3, 4, 5]", t => {
-    const testOne = getProduct([1, 2, 3, 4]);
+    const testOne = getProduct([1, 2, 3, 4, 5]);
     const expected = [120, 60, 40, 30, 20];
 
     t.is(testOne, expected);

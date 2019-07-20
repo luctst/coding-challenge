1ee94e91c4c166d50fa00b2f1c79f53c36c2396d
Lucas Tostée <lucas.tostee@gmail.com>
2019-07-20 18:58:47 +0200

    Update test

--- test/get-product.test.js
+++ test/get-product.test.js
@@ -3,14 +3,14 @@ import getProduct from "../challenges/get-products/getProducts";
 
 test("Test getProduct function with [1, 2, 3, 4, 5]", t => {
     const testOne = getProduct([1, 2, 3, 4, 5]);
-    const expected = [120, 60, 40, 30, 20];
+    const expected = [120, 60, 40, 30, 24];
 
-    t.is(testOne, expected);
+    t.deepEqual(testOne, expected);
 });
 
 test("Test getProduct function with [3, 2, 1]", t => {
     const testTwo = getProduct([3, 2, 1]);
     const expected = [2, 3, 6];
 
-    t.is(testTwo, expected);
+    t.deepEqual(testTwo, expected);
 });
